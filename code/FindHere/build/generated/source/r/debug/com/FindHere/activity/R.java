beb28c7b0dd6b84b7c2bc1c9e013ee9dbfd8273f
ASUS-PC\asus <965503692@qq.com>
2017-07-01 18:23:56 +0800

    新版UI，可实现untiy与imageview的切换

--- code/FindHere/build/generated/source/r/debug/com/FindHere/activity/R.java
+++ code/FindHere/build/generated/source/r/debug/com/FindHere/activity/R.java
@@ -20,46 +20,58 @@ public final class R {
     }
     public static final class dimen {
         public static final int about_title=0x7f060000;
-        public static final int menu_title=0x7f060001;
-        public static final int menu_title_padding=0x7f060002;
-        public static final int overlay_author=0x7f060003;
-        public static final int overlay_price_old=0x7f060004;
-        public static final int overlay_price_title=0x7f060005;
-        public static final int overlay_price_value=0x7f060006;
-        public static final int overlay_rating_text=0x7f060007;
-        public static final int overlay_status=0x7f060008;
-        public static final int overlay_title=0x7f060009;
-        public static final int overlay_touch_for_more=0x7f06000a;
+        public static final int activity_horizontal_margin=0x7f060001;
+        public static final int activity_vertical_margin=0x7f060002;
+        public static final int menu_title=0x7f060003;
+        public static final int menu_title_padding=0x7f060004;
+        public static final int overlay_author=0x7f060005;
+        public static final int overlay_price_old=0x7f060006;
+        public static final int overlay_price_title=0x7f060007;
+        public static final int overlay_price_value=0x7f060008;
+        public static final int overlay_rating_text=0x7f060009;
+        public static final int overlay_status=0x7f06000a;
+        public static final int overlay_title=0x7f06000b;
+        public static final int overlay_touch_for_more=0x7f06000c;
     }
     public static final class drawable {
-        public static final int app_banner=0x7f020000;
-        public static final int app_icon=0x7f020001;
-        public static final int scanline=0x7f020002;
-        public static final int vuforia_splash=0x7f020003;
+        public static final int add=0x7f020000;
+        public static final int app_banner=0x7f020001;
+        public static final int app_icon=0x7f020002;
+        public static final int baoman=0x7f020003;
+        public static final int camera=0x7f020004;
+        public static final int scanline=0x7f020005;
+        public static final int user=0x7f020006;
+        public static final int vuforia_splash=0x7f020007;
     }
     public static final class id {
-        public static final int about_html_text=0x7f080004;
-        public static final int about_text_title=0x7f080000;
-        public static final int add_com_btn=0x7f080002;
-        public static final int button_start=0x7f080003;
-        public static final int camera_overlay_layout=0x7f080005;
-        public static final int history_btn=0x7f080001;
-        public static final int loading_layout=0x7f080007;
-        public static final int logo_image=0x7f08000b;
-        public static final int scan_btn=0x7f080009;
-        public static final int scan_line=0x7f080008;
-        public static final int splash_image=0x7f08000a;
-        public static final int unity=0x7f080006;
+        public static final int add_btn=0x7f080006;
+        public static final int camera_btn=0x7f080005;
+        public static final int loading_layout=0x7f080002;
+        public static final int logo_image=0x7f080008;
+        public static final int main_activity=0x7f080000;
+        public static final int scan_line=0x7f080003;
+        public static final int splash_image=0x7f080007;
+        public static final int unity3d=0x7f080001;
+        public static final int user_btn=0x7f080004;
     }
     public static final class layout {
-        public static final int about_screen=0x7f030000;
-        public static final int camera_overlay=0x7f030001;
-        public static final int splash_screen=0x7f030002;
+        public static final int main_activity=0x7f030000;
+        public static final int splash_screen=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int button_start=0x7f070001;
-        public static final int splash_screen_description=0x7f070002;
+        public static final int action_sign_in=0x7f070000;
+        public static final int action_sign_in_short=0x7f070001;
+        public static final int app_name=0x7f070002;
+        public static final int button_start=0x7f070003;
+        public static final int error_field_required=0x7f070004;
+        public static final int error_incorrect_password=0x7f070005;
+        public static final int error_invalid_email=0x7f070006;
+        public static final int error_invalid_password=0x7f070007;
+        public static final int permission_rationale=0x7f070008;
+        public static final int prompt_email=0x7f070009;
+        public static final int prompt_password=0x7f07000a;
+        public static final int splash_screen_description=0x7f07000b;
+        public static final int title_activity_login=0x7f07000c;
     }
     public static final class style {
         public static final int UnityThemeSelector=0x7f040000;
--- code/FindHere/src/main/java/com/FindHere/activity/MainActivity.java
+++ code/FindHere/src/main/java/com/FindHere/activity/MainActivity.java
@@ -1,39 +1,71 @@
 package com.FindHere.activity;
 
-import com.unity3d.player.UnityPlayer;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import com.unity3d.player.UnityPlayer;
 
-public class MainActivity extends UnityPlayerActivity {
+public class MainActivity extends Activity{
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
     private LinearLayout u3dLayout;
-    private Button historyBtn,addBtn;
-    private ImageButton scanBtn;
+    private ImageButton userBtn,cameraBtn,addBtn;
+    private RelativeLayout loadLayout;
+    private View scanLine;
+    private ImageView cameraClose;
+
+    private boolean camera_on = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.camera_overlay);
-        u3dLayout = (LinearLayout) findViewById(R.id.unity);
-        u3dLayout.addView(mUnityPlayer);
-        mUnityPlayer.requestFocus();
-        scanBtn = (ImageButton) findViewById(R.id.scan_btn);
-        historyBtn = (Button) findViewById(R.id.history_btn);
-        addBtn = (Button) findViewById(R.id.add_com_btn);
+        setContentView(R.layout.main_activity);
+        mUnityPlayer = new UnityPlayer(this);
 
-        scanBtn.setOnClickListener(new OnClickListener() {
+        cameraClose = new ImageView(this);
+        cameraClose.setImageResource(R.drawable.baoman);
+        u3dLayout =  findViewById(R.id.unity3d);
+        u3dLayout.addView(cameraClose);
+        userBtn =  findViewById(R.id.user_btn);
+        cameraBtn =  findViewById(R.id.camera_btn);
+        addBtn =  findViewById(R.id.add_btn);
+        loadLayout =  findViewById(R.id.loading_layout);
+        scanLine = findViewById(R.id.scan_line);
+
+        userBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 UnityPlayer.UnitySendMessage("ForAndroid", "sayHello", "");
             }
         });
-        historyBtn.setOnClickListener(new OnClickListener() {
+        cameraBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                UnityPlayer.UnitySendMessage("Manager", "ZoomOut", "");
+                if(camera_on){
+                    mUnityPlayer.pause();
+                    u3dLayout.removeAllViews();
+                    u3dLayout.addView(cameraClose);
+                    camera_on = false;
+                }else{
+                    u3dLayout.removeAllViews();
+                    u3dLayout.addView(mUnityPlayer);
+                    mUnityPlayer.resume();
+                    mUnityPlayer.requestFocus();
+                    camera_on = true;
+                    //UnityPlayer.UnitySendMessage("Manager", "ZoomOut", "");
+                }
             }
         });
         addBtn.setOnClickListener(new OnClickListener() {
@@ -43,6 +75,85 @@ public class MainActivity extends UnityPlayerActivity {
             }
         });
     }
+
+
+    @Override protected void onNewIntent(Intent intent)
+    {
+        // To support deep linking, we need to make sure that the client can get access to
+        // the last sent intent. The clients access this through a JNI api that allows them
+        // to get the intent set on launch. To update that after launch we have to manually
+        // replace the intent with the one caught here.
+        setIntent(intent);
+    }
+
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.quit();
+        super.onDestroy();
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+        mUnityPlayer.resume();
+    }
+
+     protected void kill(){}
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
 
     /**
      * 3D调用此方法，用于退出3D
@@ -62,38 +173,33 @@ public class MainActivity extends UnityPlayerActivity {
             }
         });
     }
-    /**
-     * 按键点击事件
-     */
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            onDestroy();
-        }
-        return true;
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //UnityPlayer.UnitySendMessage("Manager", "Unload", "");
-        mUnityPlayer.quit();
-    }
-    // Pause Unity
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mUnityPlayer.pause();
-    }
-    // Resume Unity
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mUnityPlayer.resume();
-    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         // mUnityPlayer.quit();
         // this.finish();
     }
+
+  /**  @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        int[] location = new int[2];
+
+        scanLine.getLocationInWindow(location);
+
+        int left = scanLine.getLeft();
+        int top = scanLine.getTop();
+        int bottom = scanLine.getBottom();
+
+        // 从上到下的平移动画
+        Animation verticalAnimation = new TranslateAnimation(left, left, top, bottom);
+        verticalAnimation.setDuration(3000); // 动画持续时间
+        verticalAnimation.setRepeatCount(Animation.INFINITE); // 无限循环
+
+        // 播放动画
+        scanLine.setAnimation(verticalAnimation);
+        verticalAnimation.startNow();
+    }*/
 }
