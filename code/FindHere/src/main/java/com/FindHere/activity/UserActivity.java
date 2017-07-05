515bbd79c8eb666eee5b4857886660a69a47f69c
msi-PC\msi <913951645@qq.com>
2017-07-05 15:31:11 +0800

    no message

---
+++ code/FindHere/src/main/java/com/FindHere/activity/UserActivity.java
@@ -0,0 +1,56 @@
+package com.FindHere.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by msi on 2017/7/5.
+ */
+
+public class UserActivity extends Activity {
+    public UserActivity() {
+        super();
+    }
+
+    @Override
+    public Intent getIntent() {
+        return super.getIntent();
+    }
+
+    @Override
+    public void setIntent(Intent newIntent) {
+        super.setIntent(newIntent);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
